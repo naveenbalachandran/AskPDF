03f3b1cd8f4418aebe71be430c019e8d12f7d0ac
naveenbalachandran <bnaveen360@gmail.com>
2023-05-24 10:22:16 +0530

    Removed copy functionality for Streamlit Cloud
    This commit removes the copy functionality from the web app deployed on Streamlit Cloud. The Streamlit Cloud platform does not support the copy functionality, so it has been removed to ensure consistent behavior across environments. However, please note that the "Copy to Clipboard" button is still available if you run the app locally using the command "streamlit run".

---
+++ app_web.py
@@ -0,0 +1,53 @@
+import tempfile
+import os
+from extract_text import extract_text_from_pdf
+from call2llm import get_response, get_continued_response  # Import specific functions instead of importing everything from call2llm
+import streamlit as st
+
+def main():
+    st.title("AskPDF")
+
+    uploaded_files = st.sidebar.file_uploader("Upload PDF files", accept_multiple_files=True, type=["pdf"])
+    pdf_files = []
+
+    # Process uploaded PDFs
+    for uploaded_file in uploaded_files:
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(uploaded_file.read())
+            temp_file_path = temp_file.name
+
+        pdf_files.append((uploaded_file.name, temp_file_path))
+    
+    # Two-column layout for start and end pages
+    col1, col2 = st.columns(2)
+
+    # Convert PDFs to text
+    converted_texts = []
+    for i, (file_name, file_path) in enumerate(pdf_files):
+        # Input fields for start and end pages
+        with col1:
+            start_page = st.number_input(f"Start Page ({file_name})", min_value=1, value=1, key=f"start_page_{i}")
+        with col2:
+            end_page = st.number_input(f"End Page ({file_name})", min_value=start_page, value=start_page, key=f"end_page_{i}")
+
+        # Convert PDF to text
+        text = extract_text_from_pdf(file_path, start_page, end_page)
+        converted_texts.append(text)
+        print(converted_texts)
+
+    query = st.text_input("Enter your question")
+
+    if st.button("Ask PDF"):
+        response_placeholder = st.empty()
+        print("calling llm")
+        combined_text = "\n".join(converted_texts)
+        response = ""
+
+        response = get_response(combined_text + query, response_placeholder)
+    # Delete temporary files
+    for _, file_path in pdf_files:
+        os.remove(file_path)
+
+
+if __name__ == "__main__":
+    main()
